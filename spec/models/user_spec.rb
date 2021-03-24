e7bfc44d77d0c06565a42f055930e4b2008ff2dc
fisher1031 <fisher1031@outlook.jp>
2021-03-24 20:57:43 +0900

    テストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,44 @@ before do
 end
 
  describe 'ユーザー新規登録' do
+  context '新規登録がうまくいくとき' do
+    it "nicknameとemail、passwordとpassword_confirmation、first_nameとsecond_name、first_name_kanaとsecond_name_kana、birthdayが存在すれば登録できる" do
+      @user.nickname = 'ニックネーム'
+      @user.email = 'test@testertester.com'
+      @user.password = 'abc123'
+      @user.password_confirmation = 'abc123'
+      @user.first_name = '山田'
+      @user.second_name = '隆太郎'
+      @user.first_name_kana = 'ヤマダ'
+      @user.second_name_kana = 'リュウタロウ'
+      @user.birthday = '1994-10-31'
+      expect(@user).to be_valid
+    end
+    it 'passwordとpassword_confirmationが一致し、かつ6文字以上の半角英数字混合であれば登録できる' do
+      @user.password = '123abc'
+      @user.password_confirmation = '123abc'
+      expect(@user).to be_valid
+    end
+    it 'first_nameは全角(漢字・ひらがな・カタカナ)であれば登録できる' do
+      @user.first_name = '山田だダ'
+      expect(@user).to be_valid
+    end
+    it 'second_nameは全角(漢字・ひらがな・カタカナ)であれば登録できる' do
+      @user.second_name = '山田だダ'
+      expect(@user).to be_valid
+    end
+    it 'first_name_kanaは全角(カタカナ)であれば登録できる' do
+      @user.first_name_kana = 'ヤマダ'
+      expect(@user).to be_valid
+    end
+    it 'second_name_kanaは全角(カタカナ)であれば登録できる' do
+      @user.second_name_kana = 'リクタロウ'
+      expect(@user).to be_valid
+    end
+  end
+
+
+  context '新規登録がうまくいかないとき' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''  # nicknameの値を空にする
       @user.valid?
@@ -32,7 +70,7 @@ end
       @user.valid?
       expect(@user.errors.full_messages).to include "Password can't be blank"
     end
-    it 'passwordは6文字以上でないと登録できない' do
+    it 'passwordは6文字以下では登録できない' do
       @user.password = '123ab'
       @user.valid?
       expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
@@ -72,14 +110,28 @@ end
       @user.valid?
       expect(@user.errors.full_messages).to include "Second name can't be blank"
     end
-    it 'first_nameは全角(漢字・ひらがな・カタカナ)であれば登録できる' do
-      @user.first_name = '山田だダ'
-      expect(@user).to be_valid
+
+    it 'first_nameは半角英単語であれば登録できない' do
+      @user.first_name = 'abc'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name is invalid"
     end
-    it 'second_nameは全角(漢字・ひらがな・カタカナ)であれば登録できる' do
-      @user.second_name = '山田だダ'
-      expect(@user).to be_valid
+    it 'first_nameは半角数字であれば登録できない' do
+      @user.first_name = '123'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name is invalid"
     end
+    it 'second_nameは半角英単語であれば登録できない' do
+      @user.second_name = 'abc'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Second name is invalid"
+    end
+    it 'second_nameは半角数字であれば登録できない' do
+      @user.second_name = '123'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Second name is invalid"
+    end
+
     it 'first_name_kanaが空では登録できない' do
       @user.first_name_kana = ''  # first_name_kanaの値を空にする
       @user.valid?
@@ -90,13 +142,47 @@ end
       @user.valid?
       expect(@user.errors.full_messages).to include "Second name kana can't be blank"
     end
-    it 'first_name_kanaは全角(カタカナ)でなければ登録できない' do
-      @user.first_name_kana = 'ヤマダ'
-      expect(@user).to be_valid
+    it 'first_name_kanaは全角(ひらがな)であれば登録できない' do
+      @user.first_name_kana = 'やまだ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name kana is invalid"
     end
-    it 'second_name_kanaは全角(カタカナ)でなければ登録できない' do
-      @user.second_name_kana = 'リクタロウ'
-      expect(@user).to be_valid
+    it 'second_name_kanaは全角(ひらがな)であれば登録できない' do
+      @user.second_name_kana = 'りくたろう'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Second name kana is invalid"
+    end
+
+    it 'first_name_kanaは全角(漢字)であれば登録できない' do
+      @user.first_name_kana = '山田'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name kana is invalid"
+    end
+    it 'second_name_kanaは全角(漢字)であれば登録できない' do
+      @user.second_name_kana = '隆太郎'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Second name kana is invalid"
+    end
+
+    it 'first_name_kanaは半角(英単語)であれば登録できない' do
+      @user.first_name_kana = 'yamada'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name kana is invalid"
+    end
+    it 'second_name_kanaは半角(英単語)であれば登録できない' do
+      @user.second_name_kana = 'rikutarou'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Second name kana is invalid"
+    end
+    it 'first_name_kanaは半角(数字)であれば登録できない' do
+      @user.first_name_kana = '123'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name kana is invalid"
+    end
+    it 'second_name_kanaは半角(数字)であれば登録できない' do
+      @user.second_name_kana = '123'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Second name kana is invalid"
     end
     it 'birthdayが空では登録できない' do
       @user.birthday = ''  # birthdayの値を空にする
@@ -104,4 +190,5 @@ end
       expect(@user.errors.full_messages).to include "Birthday can't be blank"
     end
   end
+end
 end
