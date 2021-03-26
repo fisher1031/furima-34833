dce8fcdc70e01bc730c760bd1dc42176a1a8bab4
fisher1031 <fisher1031@outlook.jp>
2021-03-26 16:45:18 +0900

    JSの再修正

--- app/javascript/packs/item_price.js
+++ app/javascript/packs/item_price.js
@@ -4,10 +4,11 @@ priceInput.addEventListener("input", () => {
    const inputValue = priceInput.value;
 
    const addTaxDom = document.getElementById("add-tax-price");
-　　addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+   addTaxDom.innerHTML = (Math.floor(inputValue / 10));
 
    const profit = document.getElementById("profit");
-   profit.innerHTML = (Math.floor(inputValue - inputValue * 0.1));
+   const result = (Math.floor(inputValue / 10));
+   profit.innerHTML = (Math.floor(inputValue - result ))
 })
 });
 
