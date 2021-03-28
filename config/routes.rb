e1db81f2e623f054c3f477ceef3fb9290c747372
fisher1031 <fisher1031@outlook.jp>
2021-03-28 14:27:09 +0900

    商品詳細表示機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'items/index'
   root to: "items#index"
-  resources :users, only: :new
   resources :items, only: [:index, :new, :create, :show]
 end
